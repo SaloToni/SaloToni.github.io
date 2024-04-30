cd15778e833970cb0b954edde9acb4086dc5d4ba
SaloToni <143175888+SaloToni@users.noreply.github.com>
2024-04-30 10:18:26 +0300

    Update rocket.js

--- tehtävät/JS Rocket/javascript/rocket.js
+++ tehtävät/JS Rocket/javascript/rocket.js
@@ -2,7 +2,9 @@ var changeState = function (state) {
     document.body.className = 'body-state'+ state;
 
     if (state == 2) {
-        document.getElementById('countdown')
-            innerHTML = 9;
+        timer = setInterval(function () {
+            document.getElementById('countdown').innerHTML = countdownNumber;
+            countdownNumber = countdownNumber -1;
+        }, 1000);   
     };
 }
